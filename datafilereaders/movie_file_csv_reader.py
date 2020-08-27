c4d3c32f96d0a1e58f242fe2a8e06dc6a6091ded
ek360 <69452735+ek360@users.noreply.github.com>
2020-08-27 22:52:39 +1200

    Add files via upload

--- datafilereaders/movie_file_csv_reader.py
+++ datafilereaders/movie_file_csv_reader.py
@@ -9,7 +9,11 @@ class MovieFileCSVReader:
 
     def __init__(self, file_name: str):
         self.__file_name = file_name
-
+        self.__dataset_of_movies = []
+        self.__dataset_of_actors = []
+        self.__dataset_of_directors = []
+        self.__dataset_of_genres = []
+        
     def read_csv_file(self):
         with open(self.__file_name, mode='r', encoding='utf-8-sig') as csvfile:
             movie_file_reader = csv.DictReader(csvfile)
@@ -18,5 +22,30 @@ class MovieFileCSVReader:
             for row in movie_file_reader:
                 title = row['Title']
                 release_year = int(row['Year'])
-                print(f"Movie {index} with title: {title}, release year {release_year}")
+                
+                self.__dataset_of_movies += [Movie(title,release_year)]
+                
+                for act in row['Actors'].split(","):#csv file split by commas
+                    if Actor(act) not in self.__dataset_of_actors:
+                        self.__dataset_of_actors += [Actor(act)]
+                        
+                if Director(row["Director"]) not in self.__dataset_of_directors:
+                    self.__dataset_of_directors += [Director(row["Director"])]
+                
+                for gen in row['Genre'].split(","):#csv file split by commas
+                    if Genre(gen) not in self.__dataset_of_genres:
+                        self.__dataset_of_genres += [Genre(gen)]
+                
                 index += 1
+    @property         
+    def dataset_of_movies(self):
+        return self.__dataset_of_movies
+    @property
+    def dataset_of_actors(self):
+        return self.__dataset_of_actors
+    @property
+    def dataset_of_directors(self):
+        return self.__dataset_of_directors
+    @property
+    def dataset_of_genres(self):
+        return self.__dataset_of_genres
